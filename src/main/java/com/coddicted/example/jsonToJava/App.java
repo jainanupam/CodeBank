27d58ca15ec079ac2ed606aafd0c407659e9131d
Anupam Jain <anupam2204.smart@gmail.com>
2018-05-27 17:58:46 +0530

    Adding the Json to Java converter code

---
+++ src/main/java/com/coddicted/example/jsonToJava/App.java
@@ -0,0 +1,17 @@
+package com.coddicted.example.jsonToJava;
+
+import com.coddicted.gen.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Test t = new Test();
+        t.getId();
+    }
+}
